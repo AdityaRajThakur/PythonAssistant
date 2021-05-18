1f40ed7d03e07582cd27ba2ed9956cd78e6a3c83
AdityaRajThakur <adityaraj715426@gmail.com>
2021-05-18 15:08:29 +0530

    added the gui file

--- main.py
+++ main.py
@@ -1,74 +1,140 @@
 import requests
 import pyttsx3
 import speech_recognition as sr
-import wikipedia as wiki  
+import wikipedia as wiki
 import time
+import datetime
+import webbrowser
+import os
+from plyer import notification
+import random
 
-r = sr.Recognizer()
+
+r = sr.Recognizer() 
+# r.adjust_for_ambient_noise()
+# r.energy_threshold
 engine = pyttsx3.init('sapi5')
-engine.setProperty('rate',150)
+engine.setProperty('rate', 150)
 
 micname = "Microphone (WO Mic Device)"
 device_id = 0;
 sample_rate = 48000
 chunk_size = 2048*2
-down = True
+down =  True
+greet = True
+master = 'Aditya'
 mic_list = sr.Microphone.list_microphone_names()
+list = ['what', 'who', 'about']
+
 
 def get_device_id():
-    for i,mic in enumerate(mic_list):
+    for i, mic in enumerate(mic_list):
         if micname == mic:
-            device_id = i ;
+            device_id = i;
             break
+def info():
+    module_name = 'Jarvis'
+    version = 2.0
+    speak(f'Sir My name is {module_name} , version {version} ')
 
+def greet_to():
+    hour = int(datetime.datetime.now().strftime('%I'))
+   
+    minute = int(datetime.datetime.now().strftime('%M'))
+    meridian = str(datetime.datetime.now().strftime('%p'))
+    
+    if hour>=5 and hour<=12 and meridian=='AM':
+        speak(f'Good Morning {master}')
+    elif hour>=1 and hour<=6 and meridian=='PM':
+        speak(f'Good Afternoon {master}')
+    elif hour>=6 and hour<=7 and meridian=='PM':
+        speak(f'Good Evening {master}')
+    else:
+        speak(f'Good Night {master}')
+
+def ntfc(title,messag):
+    notif = notification.notify(title = title , messag = messag ,timeout = 2);
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
     # engine.save_to_file(text,'speech.mp3')    
 
-def search(text):
-                speak('searching...')
-            print('searching...')
-            text = wiki.summary(query , sentences=2)
-            print(text)
-            time.sleep(0.5)
-            speak(text)
+def opencode():
+        vscodepath = 'C:\\Users\\Administrator\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe'
+        os.startfile(vscodepath)
 
-def command
 
-while(down):
-    
-    list = ['what', 'who', 'about']
+def search():
+    speak('What do you want to search from wikipeadia')
+    query = Command()
+    speak('searching in wikipeadia')
+    text = wiki.summary(query , sentences=1)
+    speak('Search complete result may or may not be accurate')
+    print(text)
+    time.sleep(0.5)
+    speak(text)
+
+def Command():
     with sr.Microphone(device_index=device_id,sample_rate=sample_rate,chunk_size=chunk_size) as source:
-        r.adjust_for_ambient_noise(source,duration=1.5)
+        # r.adjust_for_ambient_noise(source)
         print('listening...')
+        r.pause_threshold = 1
         audio = r.listen(source)
         try:
-            print('recognizing..')
+            print('Recognizing..')
             query = r.recognize_google(audio)
-
         except sr.UnknownValueError:
             print('say again')
-            speak('say again')
+            # speak('say again')
         except sr.RequestError as e:
             print("cannot request from google wait")
-            speak("cannot request form goolge wait")
-            
-            
-    for _ in list:
-        if _ in query:
+            # speak("cannot request form goolge wait")
+            return None
+        return query
 
-            
-            
-     
-    if 'shutdown' in query:
-        print(query)
-        down = False
-        speak('shutting down the system ')
-        break;
 
-    # speak(query)
-    print(query)
+
+if __name__ == '__main__':
+    while(down):
+        query = Command()
+        if greet==True:
+            greet_to()
+            greet = False
+            
+        elif 'who are you' in query:
+            info()
+            
+        elif 'wikipeadia wiki' in query:
+            search()
+                
+        elif 'shutdown' in query:
+            print(query)
+            down = False
+            speak('shutting down the system ')
+            break;
+        
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+            
+        elif 'open youtube' in query:
+            speak('opening youtube')
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            speak('opening google')
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            speak('opening stackoverflow')
+            webbrowser.open("stackoverflow.com") 
+              
+        elif 'open code' in query:
+            speak('Openining Code ')
+            opencode();
+
     
+
+        
     
